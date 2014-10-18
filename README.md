ca4418a2212913474dc5e2dc93e68c0b1e076474
Allan <iggymacd@gmail.com>
2014-10-18 12:24:11 -0300

    in the beginning

---
+++ README.md
@@ -0,0 +1,4 @@
+hackathon1
+==========
+
+1st hackathon project
---
+++ build.dart
@@ -0,0 +1,6 @@
+import 'package:polymer/builder.dart';
+        
+main(args) {
+  build(entryPoints: ['web/hackathon_one.html'],
+        options: parseOptions(args));
+}
